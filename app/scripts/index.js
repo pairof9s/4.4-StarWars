db3c22e6c222c9485a8d05e99bdbf0c7627ca110
David Nine <pairof9s@me.com>
2016-06-09 12:30:13 -0400

    morning lecture notes

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,45 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var handlebars = require('handlebars');
+
+var githubtoken = require('./githubapikey.js');
+
+if(githubtoken !== undefined){
+  $.ajaxSetup({
+    headers: {
+      'Authorization': 'token' + githubtoken
+    }
+  });
+}
+console.log(githubtoken);
+
+var baseURL = 'http://swapi.co/api/';
+var planetListItemTemplate = $('#planet-list-item-template').html();
+var template = handlebars.compile(planetListItemTemplate);
+
+$('.js-planets-button').on('click', function(event){
+  event.preventDefault();
+  // console.log('clicked');
+  fetchPlanets();
+});
+
+function fetchPlanets(){
+  var planetsURL = baseURL + 'planets/';
+  // var returnMeSomeData = $.ajax(planetsURL);
+  // console.log(returnMeSomeData);
+  $.ajax(planetsURL).done(function(planetList){
+    // console.log(data);
+    planetList.results.forEach(function(planet){
+      displayPlanet(planet);
+    });
+  });
+}
+
+function displayPlanet(planet){
+  var html = template(planet);
+  $('.js-planet-list').append(html);
+
+  $.ajax(planet.url).done(function(planetDetails){
+    $('#' + planetDetails.name).append('<span>::' + planetDetails.climate + '<span>');
+    console.log(planetDetails);
+  });
+}
